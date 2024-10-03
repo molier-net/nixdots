4333c7824d47abe277ea5d58ea49b94d4a90194a
Remko Molier <remko.molier@googlemail.com>
2024-10-03 17:31:24 +0200

    feat: establish bluetooth baseline and add to probook-4720s as a system related setting

--- modules/nixos/hardware/bluetooth/default.nix
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -15,31 +15,25 @@
     virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
     systems, # An attribute map of your defined hosts.
 
-    # All other arguments come from the system system.
+    # All other arguments come from the module system.
     config,
     ...
 }:
-
 with lib;
 with lib.${namespace};
+let cfg = config.${namespace}.hardware.bluetooth;
+in
 {
-    imports = [
-        ./hardware-configuration.nix
-    ];
-
-    nixdots = {
-        archetypes = {
-            workstation = enabled;
-        };
-        
-        hardware.bluetooth = enabled;
+    options.${namespace}.hardware.bluetooth = with types; {
+        enable = mkBoolOpt false "Whether or not to enable bluetooth support.";
     };
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.05"; # Did you read the comment?
+    config = mkIf cfg.enable {
+        
+        # Bluetooth
+        hardware.bluetooth.enable = true;
+        hardware.bluetooth.powerOnBoot = true;
+
+
+    };
 }
